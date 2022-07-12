a01178f5769f9ac8a1256b8fd0073b2adb486128
doctornumber <doctor.ordernumber@gmail.com>
2022-07-12 18:42:39 +0200

    added new pic

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-setInterval(function() {
-    console.log("Called set Inrerval")
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            var resStr = xmlHttp;
-            console.log("EVo req: ", resStr);
-            var myImageElement = document.getElementById('myImage');
-            var div1 = document.getElementById('myDiv');
-            var parsedJsonObjResp = JSON.parse(xmlHttp.response);
-            var lastUpdated = new Date(parsedJsonObjResp.lastUpdated);
-            myImageElement.src = "data:image/png;base64, " + parsedJsonObjResp.base64Str;
-            div1.textContent = "Last updated: " + lastUpdated.toLocaleString();
-        }
-    }
-    xmlHttp.open("GET", "http://127.0.0.1:8080/api/v1/queue", true);
-    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xmlHttp.setRequestHeader('Accept', '*/*')
-    xmlHttp.send(null);
-}, 5000);
+// setInterval(function() {
+//     console.log("Called set Inrerval")
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function() {
+//         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+//             var resStr = xmlHttp;
+//             console.log("EVo req: ", resStr);
+//             var myImageElement = document.getElementById('myImage');
+//             var div1 = document.getElementById('myDiv');
+//             var parsedJsonObjResp = JSON.parse(xmlHttp.response);
+//             var lastUpdated = new Date(parsedJsonObjResp.lastUpdated);
+//             myImageElement.src = "data:image/png;base64, " + parsedJsonObjResp.base64Str;
+//             div1.textContent = "Last updated: " + lastUpdated.toLocaleString();
+//         }
+//     }
+//     xmlHttp.open("GET", "http://127.0.0.1:8080/api/v1/queue", true);
+//     xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+//     xmlHttp.setRequestHeader('Accept', '*/*')
+//     xmlHttp.send(null);
+// }, 5000);
